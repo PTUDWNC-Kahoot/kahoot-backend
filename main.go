a7a51447cfcc196955aa22bf051eed3a31fad9f0
thuytrinh1 <thuy.trinh1@tiki.vn>
2022-11-23 21:09:20 +0700

    build: set up cors middleware

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"examples/identity/db"
 	service "examples/identity/internal/service/jwthelper"
 	"examples/identity/internal/usecase"
+	"os"
 
 	authRouter "examples/identity/internal/delivery/auth"
 	v1 "examples/identity/internal/delivery/v1"
@@ -14,8 +15,9 @@ import (
 )
 
 func main() {
-
-	r := gin.Default()
+	// gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	r.Use(CORSMiddleware())
 	s := config.LoadEnvConfig()
 	db := db.InitDatabase(s)
 
@@ -32,6 +34,25 @@ func main() {
 	groupUsecase := usecase.NewGroupUsecase(groupRepo)
 	router := v1.NewRouter(jwtHandler, kahootUsecase, groupUsecase)
 	router.Register(r)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = s.Port
+	}
+	r.Run(":" + port)
+}
 
-	r.Run(":" + s.Port)
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
