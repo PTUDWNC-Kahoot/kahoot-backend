a36728ec1840f5713f394885489996b1b0b13aae
thuytrinh1 <108668264+thuytrinh1@users.noreply.github.com>
2023-01-11 23:18:17 +0700

    fix: group delete cors

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -61,7 +61,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-TB-Access-Token, accept, origin, Cache-Control, X-Requested-With, Content-Encoding")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
